ca8cb53a593e53c79f1adbcccc9e7be948256fae
RickStevenson <dadsathome1@gmail.com>
2024-09-09 10:38:42 -0600

    Finished Method Overloading Exercise

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -1,10 +1,36 @@
-﻿namespace MethodOverloading
+﻿using System.ComponentModel;
+
+namespace MethodOverloading
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string answer = Add(1,0,true);
+            Console.WriteLine(answer);
+        }
+
+        public static int Add(int x, int y)
+        {
+            return x + y;
+        }
+        public static decimal Add(decimal x, decimal y)
+        {
+            return x + y;
+        }
+        public static string Add(int x, int y, bool isDollars)
+        {
+            int result = x + y;
+
+            if (isDollars)
+            {
+                return (result > 1) ? $"{result} dollars" : $"{result} dollar";
+            }
+            else
+            {
+                return $"{result}";
+            }
+            
         }
     }
 }
--- MethodOverloading/obj/Debug/net6.0/MethodOverloading.AssemblyInfo.cs
+++ MethodOverloading/obj/Debug/net6.0/MethodOverloading.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MethodOverloading")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9297cb7c449ed80d3f815706b14d57411baba6cf")]
 [assembly: System.Reflection.AssemblyProductAttribute("MethodOverloading")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MethodOverloading")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
